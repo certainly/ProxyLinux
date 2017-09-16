b11976d065ed5f75b1b4a52fdfe97357d42d64ed
certainly <j1o2b3@gmail.com>
2017-09-16 15:57:55 +0800

    - init proxy method

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -2,24 +2,35 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
+//        get("hello") { req in
+//            var json = JSON()
+//            try json.set("hello", "world")
+//            return json
+//        }
+//
+//        get("plaintext") { req in
+//            return "Hello, world!"
+//        }
+//
+//        // response to requests to /info domain
+//        // with a description of the request
+//        get("info") { req in
+//            return req.description
+//        }
+//
+//        get("description") { req in return req.description }
         
-        try resource("posts", PostController.self)
+        get("*") { req in
+            print("heeeer")
+            print("host: \(req.uri.description)")
+            let xx: URI = req.uri
+          
+//            return "dfd"
+            return  try self.client.get(req.uri.description)
+            
+        }
+        
+        
+//        try resource("posts", PostController.self)
     }
 }
